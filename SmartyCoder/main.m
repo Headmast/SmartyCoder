bd26ea02b6429ca3be73ac1babdf56ba99932476
Kirill Klebanov <headmast@bk.ru>
2016-02-10 22:43:41 +0300

    Initial Commit

---
+++ SmartyCoder/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SmartyCoder
+//
+//  Created by Кирилл Клебанов on 10.02.16.
+//  Copyright © 2016 Kirill Klebanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
